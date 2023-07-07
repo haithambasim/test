e99a227d8788f28dba5ed6e387788c5f83f15c89
Haitham Basim <h.b.h.irs@outlook.com>
2023-07-07 22:27:14 +0300

    Update TestController.cs

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,54 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace testapp.Controllers
 {
-
     public class TestInput
     {
         public string? Name { get; set; }
         public int Age { get; set; }
     }
-
     [Route("api/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
-        // GET: api/<TestController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<TestController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST api/<TestController>
         [HttpPost]
         [Consumes("application/x-www-form-urlencoded")]
         public TestInput Post([FromForm] TestInput value)
         {
-
             return value;
-        }
-
-        // PUT api/<TestController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<TestController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
